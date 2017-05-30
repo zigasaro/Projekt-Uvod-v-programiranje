190aca73994e2c0e9dec45bcb4304c7fe4b20139
Žiga Šaranović <ziga.saranovic@hotmail.com>
2017-05-30 16:52:06 +0200

    Add files via upload

---
+++ 4 v vrsto.py
@@ -0,0 +1,7 @@
+from tkinter import*
+
+class stiri_v_vrsto:
+
+    def __init__(self, master):
+        self.platno = Canvas(master, width=500, height=500)
+        self.platno.grid(row=1, column=1)
