6e5b56c7196fd2de205c0977bced1a9057d628cd
Žiga Šaranović <ziga.saranovic@hotmail.com>
2017-05-30 17:24:45 +0200

    Add files via upload

--- 4 v vrsto.py
+++ 4 v vrsto.py
@@ -5,15 +5,16 @@ class stiri_v_vrsto:
     def __init__(self, master):
         
         self.platno = Canvas(master, width=1050, height=900)
-        self.platno.grid(row=2, column=1, columnspan=3)
+        self.platno.grid(row=2, column=1)
+        self.platno.bind('<Button-1>', self.klik)
         
         menu=Menu(master)
         master.config(menu=menu)
         menu.add_command(label='Zapri', command=master.destroy)
         menu.add_command(label='Nova igra', command=self.novaigra)
 
-        rezultat=Label(master, text='rezultat')
-        rezultat.grid(row=1, column=1)
+        naslov=Label(master, text='4 v vrsto')
+        naslov.grid(row=1, column=1)
 
         
 
@@ -21,6 +22,10 @@ class stiri_v_vrsto:
 
     def novaigra(self):
         self.novaigra = self.platno.delete(ALL)
+
+    def klik(self, event):
+        self.x=event.x
+        self.y=event.y
 
         
 
